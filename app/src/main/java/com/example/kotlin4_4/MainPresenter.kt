b262e2ca77d09b8cbbaa64eed5cbe10ace2b188a
petrdixon <crossout20001@mail.ru>
2023-07-25 12:52:53 +0300

    Урок 4. Сделал проект на MVP, MOXY. Конвертация jpg в png

---
+++ app/src/main/java/com/example/kotlin4_4/MainPresenter.kt
@@ -0,0 +1,17 @@
+package com.example.kotlin4_4
+
+import moxy.MvpPresenter
+
+class MainPresenter (val converter: Converter) : MvpPresenter<MainView>() { // converter это Model
+    override fun onFirstViewAttach() { // работает только при запуске приложения/первом присоединении View
+    }
+    override fun attachView(view: MainView?) { // работает при запуске приложения и поворотах экрана/при каждом присоединении View
+        super.attachView(view)
+    }
+    fun startConvert(){
+        val textStatus = converter.convertJpgToPng() // запускаю конвертацию файла
+        viewState.displayStatus(textStatus) // показываю статус конвертации на экране
+    }
+
+
+}
